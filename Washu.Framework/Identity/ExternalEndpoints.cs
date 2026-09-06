0286c69f0090fbebb3426bdf2e9f913380377a94
washu <washutshotheli8@gmail.com>
2026-09-06 18:11:44 +0200

    Added the 'Remember Me' system, where the checkbox is in SignInCard.razor Added a new query parameter: "PersistCookie" on the external endpoints and the registration route

--- Washu.Framework/Identity/ExternalEndpoints.cs
+++ Washu.Framework/Identity/ExternalEndpoints.cs
@@ -28,33 +28,34 @@ where TDbContext : ApplicationDbContext
         group.MapPost(CoreEndpointRoutes.External.CompleteRegistration, CompleteRegistration);
     }
 
-    private ChallengeHttpResult ChallengeExternal([FromQuery] string returnUrl)
+    private ChallengeHttpResult ChallengeExternal([FromQuery] bool persistCookie, [FromQuery] string returnUrl)
     {
         var properties =
             signInManager.ConfigureExternalAuthenticationProperties(
                 GoogleDefaults.AuthenticationScheme,
-                $"{CoreEndpointRoutes.External.ExternalCallback}?ReturnUrl={returnUrl}");
+                $"{CoreEndpointRoutes.External.ExternalCallback}?PersistCookie={persistCookie}&&ReturnUrl={returnUrl}");
 
         return TypedResults.Challenge(
             properties,
             [GoogleDefaults.AuthenticationScheme]);
     }
 
-    private async Task<IResult> ExternalCallback([FromQuery] string returnUrl)
+    private async Task<IResult> ExternalCallback([FromQuery] bool persistCookie, [FromQuery] string returnUrl)
     {
         var info = await signInManager.GetExternalLoginInfoAsync();
-        if (info is null) return Results.Redirect($"{ApplicationRoutes.SignIn}?ReturnUrl={returnUrl}");
+        if (info is null) return Results.Redirect($"{ApplicationRoutes.SignIn}?PersistCookie={persistCookie}&&ReturnUrl={returnUrl}");
         // 1. Sign in if Google link exists
         var result = await signInManager.ExternalLoginSignInAsync(
             info.LoginProvider, 
             info.ProviderKey, 
-            isPersistent: false);
+            isPersistent: persistCookie);
         return Results.Redirect(result.Succeeded ? returnUrl :
-            $"{ApplicationRoutes.Register}?ReturnUrl={returnUrl}");
+            $"{ApplicationRoutes.Register}?PersistCookie={persistCookie}&&ReturnUrl={returnUrl}");
     }
 
     private async Task<RedirectHttpResult> CompleteRegistration(
         [FromForm] Registration.Model request,
+        [FromQuery] bool persistCookie,
         [FromQuery] string returnUrl,
         HttpContext context,
         MessageStore store)
@@ -143,7 +144,7 @@ where TDbContext : ApplicationDbContext
                 await dbContext.UserRoles.AddAsync(userRole);
             }
             await dbContext.SaveChangesAsync();
-            await signInManager.SignInAsync(user, isPersistent: false);
+            await signInManager.SignInAsync(user, isPersistent: persistCookie);
             
             return TypedResults.Redirect(returnUrl);
     }
