adacd261f357d7a229b1df86dc9fde8ffc7818bf
washu <washutshotheli8@gmail.com>
2026-09-06 21:30:05 +0200

    Removed default parameter values for Message.Error, Message.Info, and Message.Success methods

--- Washu.Framework/Notifications/Message.cs
+++ Washu.Framework/Notifications/Message.cs
@@ -6,13 +6,13 @@ public readonly struct Message
     public string Title { get; init;  }
     public string Details { get; init; }
 
-    public static Message Error(string title = "", string details = "")
+    public static Message Error(string title, string details)
     => new() { Type = MessageType.Error, Title = title, Details = details };
     
-    public static Message Info(string title = "", string details = "")
+    public static Message Info(string title, string details)
         => new() { Type = MessageType.Info, Title = title, Details = details };
     
-    public static Message Success(string title = "", string details = "")
+    public static Message Success(string title, string details)
         => new() { Type = MessageType.Success, Title = title, Details = details };
 }
 
