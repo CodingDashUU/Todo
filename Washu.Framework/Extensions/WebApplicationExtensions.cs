975346d679efabc74d0005c3e46dc901bbd7fb9b
washu <washutshotheli8@gmail.com>
2026-09-07 15:43:12 +0200

    Removed unused extension methods in WebApplicationExtensions.cs

--- Washu.Framework/Extensions/WebApplicationExtensions.cs
+++ Washu.Framework/Extensions/WebApplicationExtensions.cs
@@ -21,24 +21,6 @@ public static class WebApplicationExtensions
             var roleManager = serviceScope.ServiceProvider.GetRequiredService<ApplicationRoleManager<TDbContext>>();
             await roleManager.AddRolesAsync(roles);
         }
-        public async Task SeedPermissionsAsync(ImmutableArray<string> roles, ImmutableDictionary<string, string[]> permissions)
-        {
-            using var serviceScope = app.Services.CreateScope();
-            throw new NotSupportedException();
-            // var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
-            //
-            // foreach (var role in roles)
-            // {
-            //     var identityRole = await roleManager.FindByNameAsync(role);
-            //     if (identityRole is null) continue;
-            //
-            //     var existingClaims = await roleManager.GetClaimsAsync(identityRole);
-            //
-            //     foreach (var permission in permissions[role]) 
-            //         if (!existingClaims.Any(c => c.Type == "permission" && c.Value == permission))
-            //             await roleManager.AddClaimAsync(identityRole, new Claim("permission", permission));
-            // }
-        }
         public void MapThemeEndpoint() =>
             app.MapPost(CoreEndpointRoutes.Theme, (HttpContext context, [FromForm] string theme) =>
                 {
