757d029a7da2bf5fb0556c0253f75ee6b2174605
washu <washutshotheli8@gmail.com>
2026-09-12 19:31:43 +0200

    Removed unused references

--- Washu.Framework/Identity/ApplicationDbContext.cs
+++ Washu.Framework/Identity/ApplicationDbContext.cs
@@ -2,7 +2,6 @@
 
 using Entities;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 public class ApplicationDbContext(DbContextOptions options)
