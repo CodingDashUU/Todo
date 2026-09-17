e4452784b520a68954ed9ea87ec90b8aaca67583
washu <washutshotheli8@gmail.com>
2026-09-17 06:32:17 +0200

    Removed unused dependancies

--- Washu.Framework/Identity/Entities/UserRole.cs
+++ Washu.Framework/Identity/Entities/UserRole.cs
@@ -4,7 +4,7 @@ public class UserRole
 {
     public Guid Id { get; set; } = Guid.CreateVersion7();
     public Guid UserId { get; set; }
-    public ApplicationUser User { get; set; }
+    public ApplicationUser User { get; set; } = null!;
     public Guid RoleId { get; set; }
-    public ApplicationRole Role { get; set; }
+    public ApplicationRole Role { get; set; } = null!;
 }
